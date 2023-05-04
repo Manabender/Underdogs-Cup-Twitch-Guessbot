55db625b85f9707eb5dcdb76e04279c86fff9d11
25Pi25 <varunmash@outlook.com>
2023-05-04 14:09:19 -0700

    forgot to remove debug commands oops

--- commands/debug/testController.js
+++
@@ -1,3 +0,0 @@
-import { hasElevatedPermissions, twitchChat } from '../../util.js';
-
-export default ({ username }) => hasElevatedPermissions(username) && twitchChat(`${username} is a successfully registered bot controller.`);
